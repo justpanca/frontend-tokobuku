447ab406c0cda3aab34a85c87db9641e1775cf0c
justpanca <sajapanca6@gmail.com>
2025-02-22 17:27:06 +0700

    edit price in card product

--- src/components/CardProduct.vue
+++ src/components/CardProduct.vue
@@ -9,9 +9,10 @@
     </figure>
     <div class="card-body">
       <h2 class="card-title capitalize font-mono font-bold bg-gradient-to-r from-blue-600 
-      to-purple-700 bg-clip-text text-transparent  ">{{ data.name }}</h2>
+      to-purple-700 bg-clip-text text-transparent">{{ data.name }}</h2>
       <p>{{ data.description }}</p>
-      <p>{{ formatter.format(data.price) }}</p>
+      <p class="card-title capitalize font-mono font-bold bg-gradient-to-r from-blue-600 
+      to-purple-700 bg-clip-text text-transparent">{{ formatter.format(data.price) }}</p>
       <p>stock: {{ data.stock }}</p>
       <div class="card-actions justify-end">
         <RouterLink :to="`/product/${data.id}`" class="btn btn-primary"
